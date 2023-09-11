b81024343eebaa3d9984306aee8ee8d9f3ef1e0a
Rafael Dourado Bastos de Oliveira <103436880+dourado-rafa@users.noreply.github.com>
2023-09-11 17:58:48 -0300

    Update DataBase and Form Post Note

--- notes/views.py
+++ notes/views.py
@@ -1,4 +1,16 @@
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from .models import Note
+
 
 def index(request):
-    return HttpResponse("Olá mundo! Este é o app notes de Tecnologias Web do Insper.")
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        content = request.POST.get('content')
+        print(title, content)
+        note = Note(title=title, content=content)
+        note.save()
+        
+        return redirect('index')
+    else:
+        all_notes = Note.objects.all()
+        return render(request, 'notes/index.html', {'notes': all_notes})
